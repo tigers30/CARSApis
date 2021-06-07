c5769bc4231fbd7e7d938390ef72fc9bf5e8830d
tigers30 <44579588+tigers30@users.noreply.github.com>
2021-06-07 15:44:56 -0400

    Removed comments

--- ExternalAPI/API_helper.js
+++ ExternalAPI/API_helper.js
@@ -1,11 +1,7 @@
 const request = require('request')
 
 module.exports = {
-    /*
-    ** This method returns a promise
-    ** which gets resolved or rejected based
-    ** on the result from the API
-    */
+    
     make_API_call : function(url){
         return new Promise((resolve, reject) => {
             request(url, { json: true }, (err, res, body) => {
